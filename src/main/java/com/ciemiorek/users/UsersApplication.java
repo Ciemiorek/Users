b1bb0d1b11c8fe4f2baa756a6fb25ab173b703e5
ciemiorek <artur.ciemiorek@gmail.com>
2020-06-23 13:00:44 +0200

    Add source of message

--- src/main/java/com/ciemiorek/users/UsersApplication.java
+++ src/main/java/com/ciemiorek/users/UsersApplication.java
@@ -2,8 +2,10 @@ package com.ciemiorek.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:message.properties", encoding = "UTF-8")
 public class UsersApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ciemiorek/users/common/MsgSource.java
+++ src/main/java/com/ciemiorek/users/common/MsgSource.java
@@ -6,10 +6,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class MsgSource {
 
-    private final String OK001;
+    public final String OK001;
+    public final String OK002;
+    public final String OK003;
 
-    public MsgSource(@Value("${common.ok.msg.ok001}") String ok001MsgValue) {
+
+    public final ConstErrorMsg Err001;
+    public final ConstErrorMsg Err002;
+
+    public MsgSource(
+            @Value("${common.ok.msg.ok001}") String ok001MsgValue,
+            @Value("${common.ok.msg.ok002}") String ok002MsgValue,
+            @Value("${common.ok.msg.ok003}") String ok003MsgValue,
+
+            @Value("${common.const.error.msg.err001}") String err001MsgValue,
+            @Value("${common.const.error.msg.err002}") String err002MsgValue
+
+    ) {
+
+
         OK001 = ok001MsgValue;
+        OK002 = ok002MsgValue;
+        OK003 = ok003MsgValue;
+
+        Err001 = new ConstErrorMsg("ERR001", err001MsgValue);
+        Err002 = new ConstErrorMsg("ERR002", err002MsgValue);
+
 
     }
 }
